8b2d96378ef961ea0114ce0a53ba3b67be5bf892
Ivan Zakharov <Ivan.Zakharov.aob@gmail.com>
2020-10-26 19:49:07 +0300

    Add project files.

---
+++ SimpleClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SimpleClient
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Устанавливаем подключение...");
+            socket.Connect("127.0.0.1", 909);
+            Console.WriteLine("Подключение успешно установлено.");
+
+            Console.WriteLine("Введите ваше сообщение:");
+            string message = Console.ReadLine();
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            socket.Send(buffer);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SimpleServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace SimpleServer
+{
+    class Program
+    {
+        static Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        static void Main(string[] args)
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Any, 909));
+            socket.Listen(0);
+
+            var clientSocket = socket.Accept();
+            Console.WriteLine("Новое подключение.");
+            byte[] buffer = new byte[1024];
+            clientSocket.Receive(buffer);
+
+            Console.WriteLine("Сервер получил следующее сообщение:");
+            Console.WriteLine(Encoding.UTF8.GetString(buffer));
+            Console.ReadLine();
+
+        }
+    }
+}
